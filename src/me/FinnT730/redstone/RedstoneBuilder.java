849975f302b72ded8f8e69fdf358ceb0f533fbed
FinnT730 <finnt730@gmail.com>
2016-02-23 20:08:34 +0100

    Update?

--- src/me/FinnT730/redstone/RedstoneBuilder.java
+++ src/me/FinnT730/redstone/RedstoneBuilder.java
@@ -1,17 +1,34 @@
 package me.FinnT730.redstone;
 
+import java.io.File;
+import java.util.Random;
 import java.util.logging.Logger;
 
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.libs.jline.internal.Configuration;
+import org.bukkit.entity.FallingBlock;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
 
 import me.FinnT730.redstone.loop.RedstoneLoopSmall;
 
 public class RedstoneBuilder extends JavaPlugin implements Listener {
 	
 	
+	static Random rand;
+	
+	
 	public void onEnable() {
+		
+		
 		
 		/*Command section
 		*/	
@@ -23,6 +40,50 @@ public class RedstoneBuilder extends JavaPlugin implements Listener {
 		Logger logger = Logger.getLogger("Minecraft");
 		logger.info(pdfFile.getName() + "has been enabled " + pdfFile.getVersion());		
 	}
+	
+	@SuppressWarnings("deprecation")
+	@EventHandler
+	public void onBlockPlace(BlockPlaceEvent event) {
+		if(event.getBlock().getLocation().subtract(0,1,0).getBlock().getType() == Material.AIR) {
+			event.getBlock().getWorld().spawnFallingBlock(event.getBlock().getLocation(), event.getBlock().getType(), event.getBlock().getData());
+			event.getBlock().setType(Material.AIR);
+			
+			return;
+		}
+	}
+	
+	
+	/*@SuppressWarnings("deprecation")
+	@EventHandler
+	public void onBreakBlock(BlockBreakEvent event) {
+		for(int m = 0;m != event.getBlock().getLocation().getBlockY(); m++) {
+			
+		if(event.getBlock().getLocation().add(0,1,0).getBlock().getType() != Material.AIR) {
+			if(event.getBlock().getLocation().subtract(0,1,0).getBlock().getType() == Material.AIR) {
+				event.getBlock().getWorld().spawnFallingBlock(event.getBlock().getLocation(), event.getBlock().getType(), event.getBlock().getData());
+				event.getBlock().setType(Material.AIR);
+				return;
+			}
+		}
+	}
+}*/
+	
+	
+	@EventHandler
+	public void onEntityExplode(EntityExplodeEvent event) {
+		for(Block b : event.blockList()) {
+			float x = (float) -1 + (float) (Math.random()* ((1 - -1) + 1));
+			float y = (float) -2 + (float) (Math.random()* ((2 - -2) + 1));
+			float z = (float) -1 + (float) (Math.random()* ((1 - -1) + 1));
+			
+			FallingBlock fallingBlock = b.getWorld().spawnFallingBlock(b.getLocation(), b.getType(), b.getData());
+			fallingBlock.setDropItem(false);
+			//fallingBlock.setVelocity(new Vector(x,y,z));
+			
+			b.setType(Material.AIR);
+		}
+	}
+	
 	
 	@Override
 	public void onDisable() {
--- src/me/FinnT730/redstone/loop/RedstoneLoopSmall.java
+++ src/me/FinnT730/redstone/loop/RedstoneLoopSmall.java
@@ -3,17 +3,24 @@ package me.FinnT730.redstone.loop;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import me.FinnT730.redstone.RedstoneBuilder;
 
 public class RedstoneLoopSmall implements CommandExecutor {
-
-	public RedstoneLoopSmall() {
-		// TODO Auto-generated constructor stub
-	}
-
+	
+	@SuppressWarnings("deprecation")
 	@Override
-	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean onCommand(CommandSender sender, Command command, String raw, String[] arg) {
+		if(sender instanceof Player) {
+			Player player = (Player)sender;
+			
+			ItemStack diamond = new ItemStack(0);
+			
+			player.getInventory().addItem(diamond);
+		}
+		return true;
 	}
 
 }
