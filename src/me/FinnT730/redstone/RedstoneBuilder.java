0f6ea0ab891c88e7e9e725dd057d3c8356b9dcf0
FinnT730 <finnt730@gmail.com>
2016-02-23 15:47:14 +0100

    Plugin works now with no use command.....

--- src/me/FinnT730/redstone/RedstoneBuilder.java
+++ src/me/FinnT730/redstone/RedstoneBuilder.java
@@ -2,16 +2,23 @@ package me.FinnT730.redstone;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import me.FinnT730.redstone.BlockHandler.MainBlockGet;
+import me.FinnT730.redstone.loops.RedstoneLoopSmall;
 import net.minecraft.server.v1_8_R3.Blocks;
+import net.minecraft.server.v1_8_R3.Item;
 
 public class RedstoneBuilder extends JavaPlugin {
+	
+	
+	static MainBlockGet BlockList = new MainBlockGet();
 	
 	@Override
 	public void onEnable() {
 		
+		this.getCommand("loop").setExecutor(new RedstoneLoopSmall());
 		
 		try {
-			
+			BlockList.add(Item.REGISTRY.toString());
 		} catch (Exception e) {
 			e.equals(Blocks.REDSTONE_WIRE.I);
 		}
---
+++ src/me/FinnT730/redstone/loops/RedstoneLoopSmall.java
@@ -0,0 +1,15 @@
+package me.FinnT730.redstone.loops;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class RedstoneLoopSmall implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String raw, String[] args) {
+		
+		return true;
+	}
+
+}
