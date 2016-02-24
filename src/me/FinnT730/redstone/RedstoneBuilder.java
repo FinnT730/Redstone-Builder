119c5e6c5ebfd4b54be670e7e35c498f4b332d6d
FinnT730 <finnt730@gmail.com>
2016-02-24 21:10:00 +0100

    efefefefef

--- src/me/FinnT730/redstone/RedstoneBuilder.java
+++ src/me/FinnT730/redstone/RedstoneBuilder.java
@@ -1,23 +1,20 @@
 package me.FinnT730.redstone;
 
-import java.io.File;
 import java.util.Random;
 import java.util.logging.Logger;
 
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.craftbukkit.libs.jline.internal.Configuration;
+import org.bukkit.craftbukkit.libs.jline.internal.Log.Level;
 import org.bukkit.entity.FallingBlock;
+import org.bukkit.entity.ThrownPotion;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.Vector;
-
 import me.FinnT730.redstone.loop.RedstoneLoopSmall;
 
 public class RedstoneBuilder extends JavaPlugin implements Listener {
@@ -38,12 +35,13 @@ public class RedstoneBuilder extends JavaPlugin implements Listener {
 		getServer().getPluginManager().registerEvents(this, this);		
 		PluginDescriptionFile pdfFile = getDescription();
 		Logger logger = Logger.getLogger("Minecraft");
-		logger.info(pdfFile.getName() + "has been enabled " + pdfFile.getVersion());		
+		logger.info(pdfFile.getName() + "has been enabled " + pdfFile.getVersion());
+		logger.log(null);
 	}
 	
 	@SuppressWarnings("deprecation")
 	@EventHandler
-	public void onBlockPlace(BlockPlaceEvent event) {
+	public void onBlockPlace(BlockPlaceEvent event) {		
 		if(event.getBlock().getLocation().subtract(0,1,0).getBlock().getType() == Material.AIR) {
 			event.getBlock().getWorld().spawnFallingBlock(event.getBlock().getLocation(), event.getBlock().getType(), event.getBlock().getData());
 			event.getBlock().setType(Material.AIR);
@@ -53,22 +51,20 @@ public class RedstoneBuilder extends JavaPlugin implements Listener {
 	}
 	
 	
-	/*@SuppressWarnings("deprecation")
+	@SuppressWarnings("deprecation")
 	@EventHandler
 	public void onBreakBlock(BlockBreakEvent event) {
-		for(int m = 0;m != event.getBlock().getLocation().getBlockY(); m++) {
-			
 		if(event.getBlock().getLocation().add(0,1,0).getBlock().getType() != Material.AIR) {
 			if(event.getBlock().getLocation().subtract(0,1,0).getBlock().getType() == Material.AIR) {
 				event.getBlock().getWorld().spawnFallingBlock(event.getBlock().getLocation(), event.getBlock().getType(), event.getBlock().getData());
 				event.getBlock().setType(Material.AIR);
 				return;
-			}
 		}
 	}
-}*/
+}
 	
 	
+	@SuppressWarnings("deprecation")
 	@EventHandler
 	public void onEntityExplode(EntityExplodeEvent event) {
 		for(Block b : event.blockList()) {
